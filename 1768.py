747855e489158267dab5dd6145dae902eff07721
Parth Patel <patelparthmail1996@gmail.com>
2023-10-22 18:42:08 -0700

    optimized code

--- 1768.py
+++ 1768.py
@@ -5,17 +5,14 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        i,j=0,0
-        ans =""
-        while True:
-            if i < len(word1) :
-                ans += word1[i]
-            if j < len(word2) :
-                ans += word2[j]
-            if i >= len(word1) and i >= len(word2) : 
-                break
+        result = []
+        i= 0
+        while i < len(word1) or i < len(word2):
+            if i< len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
             i+=1
-            j+=1
-        return ans
+        return ''.join(result)
 
 print(Solution().mergeAlternately("abc","pqr"))
